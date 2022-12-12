35ffe7ea44826c3fdb8839b943d316aa926f3f95
Isaac <111931906+rivera7r@users.noreply.github.com>
2022-12-12 11:33:08 -0400

    Update main.cpp

--- Tempsensor/src/main.cpp
+++ Tempsensor/src/main.cpp
@@ -5,12 +5,12 @@
 #define PIN_ANALOG_IN_TEMPERATURE 36
 #define PIN_ANALOG_IN_INTERRUPTOR 32
 
-// WIFI Credentials
-#define WIFI_NAME "Avocado Stealers"
-#define WIFI_PASSWORD "bwt7nnpqQnnh"
+// WIFI Info
+#define WIFI_NAME "RUMNET"
+#define WIFI_PASSWORD "Colegio2019"
 #define WIFI_TIMEOUT 10000
 
-// MQTT BROKER
+// MQTT BROKER Info
 const char *mqttServer = "tempsensor.space";
 const char *TempC = "tempC";
 const char *TempF = "tempF";
@@ -19,15 +19,15 @@ const int mqttPort = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-int calibrationTime = 0;
+int calibration = 0;
 bool recalibrated = false;
 float recalibratedVoltage = 0;
 float temperatureQuotient = 0;
 float recalibratingVals[5];
 
-float recalibrate(float values[5]);
 float highestTemp(float values[5]);
 float lowestTemp(float values[5]);
+float recalibrate(float values[5]);
 void connectToWifi();
 
 void connectToWifi()
@@ -89,9 +89,9 @@ void loop()
   float adcValue = analogRead(PIN_ANALOG_IN_TEMPERATURE);
   float voltage = (float)adcValue / 4095.0 * 3.3;                 // read ADC pin and convert to voltage
   float Rt = 10 * voltage / (3.3 - voltage);                      // calculate resistance value of thermistor
-  double tempK = 1 / (1 / (273.15 + 25) + log(Rt / 10) / 3950.0); // calculate temperature (Kelvin)
-  double tempC = tempK - 273.15;                                         // calculate temperature (Celsius)
-  double tempF = tempC * 9 / 5 + 32;                                    //calculate temperature (Farenheit)
+  double tempK = 1 / (1 / (273.15 + 25) + log(Rt / 10) / 3950.0); // calculate temperature  in Kelvin
+  double tempC = tempK - 273.15;                                         // calculate temperature in Celsius
+  double tempF = tempC * 9 / 5 + 32;                                    //calculate temperature inFarenheit
   double button = analogRead(PIN_ANALOG_IN_INTERRUPTOR);
 
   if (temperatureQuotient != 0)
@@ -99,18 +99,18 @@ void loop()
     tempC = tempC * temperatureQuotient;
   }
 
-  if (button == 0 && calibrationTime < 5 && recalibrated == false)
+  if (button == 0 && calibration < 5 && recalibrated == false)    //button pressed
   {
     Serial.println("Recalibrating...");
-    recalibratingVals[calibrationTime] = tempC;
-    calibrationTime++;
+    recalibratingVals[calibration] = tempC;
+    calibration++;
   }
 
-  else if (button != 0 && calibrationTime >= 5)
+  else if (button != 0 && calibration >= 5)           //without pressing button
   {
     temperatureQuotient = recalibrate(recalibratingVals);
     recalibrated = false;
-    calibrationTime = 0;
+    calibration = 0;
 
     for (int i = 0; i < 5; i++)
     { // reset array
@@ -118,7 +118,7 @@ void loop()
     }
     delay(500);
   }
-  Serial.printf("Calibration: %d  \tTemperature in C: %.2fC  \tTemperature in F: %.2fF\n", calibrationTime, tempC, tempF);
+  Serial.printf("Calibration: %d  \tTemperature : %.2fC    %.2fF\n", calibration, tempC, tempF);  //print values of tempetature
   delay(1000);
 
   if (!client.connected())
@@ -136,8 +136,7 @@ float recalibrate(float values[])
   float highestTemperature = highestTemp(values);
   float averageTemperature = (lowestTemperature + highestTemperature) / 2;
 
-  // Linear interpolation to find the temperature of the thermistor
-  float interTemp = lowestTemperature + ((5 / 2) - 1) * (highestTemperature - lowestTemperature) / (5 - 1);
+  float interTemp = lowestTemperature + ((5 / 2) - 1) * (highestTemperature - lowestTemperature) / (5 - 1);//Interpolation to find the temperature of the thermistor
   float temperatureQuotient = interTemp / averageTemperature;
 
   return temperatureQuotient;
